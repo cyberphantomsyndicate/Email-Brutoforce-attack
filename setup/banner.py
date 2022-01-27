7483fac10989589e21de47fcaa1a40c9de6182a4
hacker6291 <98105494+hacker6291@users.noreply.github.com>
2022-01-27 08:31:13 -0800

    Add files via upload

---
+++ setup/banner.py
@@ -0,0 +1,58 @@
+import os
+import platform
+from setup import colors
+from setup.colors import r,g,y,c
+
+logo = f"""
+ ██████╗ ███╗   ███╗ █████╗ ██╗██╗        
+██╔════╝ ████╗ ████║██╔══██╗██║██║        
+██║  ███╗██╔████╔██║███████║██║██║        
+██║   ██║██║╚██╔╝██║██╔══██║██║██║        
+╚██████╔╝██║ ╚═╝ ██║██║  ██║██║███████╗   
+ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚══════╝   
+                                          
+██████╗ ██████╗ ██╗   ██╗████████╗███████╗
+██╔══██╗██╔══██╗██║   ██║╚══██╔══╝██╔════╝
+██████╔╝██████╔╝██║   ██║   ██║   █████╗  
+██╔══██╗██╔══██╗██║   ██║   ██║   ██╔══╝  
+██████╔╝██║  ██║╚██████╔╝   ██║   ███████╗
+╚═════╝ ╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚══════╝
+                             {c + "coded By: "+y +"QADIR AHMAD"}             
+                                                                                                                   
+"""
+c = colors
+try:
+    from colorama import Fore, Style
+except ModuleNotFoundError:
+    os.system("pip install colorama")
+
+def banner():
+    print(c.ran + logo)
+    print(c.ran + '-'*63)
+    print("|" + "*"* 60 + c.ran + "|")
+
+    print(c.ran,"\n" + "|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX, "- " * 4, " [+] Follow me on Instagram @qadirahmad43 ", "- " * 4 + c.ran + "|")
+    print(c.ran, "\n" + "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, " [+] Follow me on Instagram @qadirahmad662 ", "- " * 4+c.ran + "|")
+    print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] Github: https://github.com/hacker629 ", "- " * 3+c.ran + "|")
+    print(c.ran + "\n"+ "|" + "*" * 60+c.ran + "|")
+
+    print(c.ran + '-' * 63)
+
+
+def banner2():
+    print(c.ran + '-'*63)
+    print("|" + "*"* 60 + c.ran + "|")
+
+    print(c.ran,"\n" + "|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX, "- " * 4, " [+] Follow me on Instagram @qadirahmad43 ", "- " * 4 + c.ran + "|")
+    print(c.ran, "\n" + "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, " [+] Follow me on Instagram@qadirahmad662", "- " * 4+c.ran + "|")
+    print(c.ran , "\n"+ "|"+ Style.BRIGHT + Fore.LIGHTRED_EX, "- " * 4, "[+] Github:  https://github.com/hacker6291  ", "- " * 3+c.ran + "|")
+    print(c.ran + "\n"+ "|" + "*" * 60+c.ran + "|")
+
+    print(c.ran + '-' * 63)
+
+def clear():
+    s = platform.platform()
+    if "Windows" in s:
+        os.system("cls")
+    else:
+        os.system("clear")
